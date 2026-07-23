d2c5ab8e86e8e6eb44c6bb243586c22d48ab5c41
maen1977 <maenish_ai@proton.me>
2026-07-23 13:49:55 +0300

    Set OrthodoxPrayers version to 5.0.15

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "com.orthodoxprayers.privateapp"
         minSdk = 26
         targetSdk = 36
-        versionCode = 50014
-        versionName = "5.0.14"
+        versionCode = 50015
+        versionName = "5.0.15"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
